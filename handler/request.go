7f76205f4ebb3de37223dd499347aad911e6a418
p0643722 <joaquim.lordelo@portoseguro.com.br>
2023-10-08 09:48:04 -0300

    createOpening handler request response

---
+++ handler/request.go
@@ -0,0 +1,43 @@
+package handler
+
+import "fmt"
+
+func errorParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+// CreateOpening
+
+type CreateOpeningRequest struct {
+	Role     string `json: "role"`
+	Company  string `json: "company"`
+	Location string `json: "location"`
+	Remote   *bool  `json: "remote"`
+	Link     string `json: "link"`
+	Salary   int64  `json: "salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Link == "" && r.Remote == nil && r.Salary <= 0 {
+		return fmt.Errorf("request body is empty or malformed")
+	}
+	if r.Role == "" {
+		return errorParamIsRequired("role", "string")
+	}
+	if r.Company == "" {
+		return errorParamIsRequired("company", "string")
+	}
+	if r.Location == "" {
+		return errorParamIsRequired("location", "string")
+	}
+	if r.Link == "" {
+		return errorParamIsRequired("link", "string")
+	}
+	if r.Remote == nil {
+		return errorParamIsRequired("remote", "bool")
+	}
+	if r.Salary <= 0 {
+		return errorParamIsRequired("salary", "int64")
+	}
+	return nil
+}
